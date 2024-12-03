b36a54c28d4054a14072728c4f6f9545c9a2a32e
CarolHall-NCSU-CBE <95452316+CarolHall-NCSU-CBE@users.noreply.github.com>
2024-12-03 17:49:46 -0500

    Add files via upload

---
+++ xdr_xtc_lib/libgmxfort/build/src/CMakeFiles/gmxfort.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../lib/libgmxfort.pdb"
+  "../lib/libgmxfort.so"
+  "../lib/libgmxfort.so.0.4"
+  "../lib/libgmxfort.so.0.4.4"
+  "CMakeFiles/gmxfort.dir/common.f90.o"
+  "CMakeFiles/gmxfort.dir/indexfile.f90.o"
+  "CMakeFiles/gmxfort.dir/trajectory.f90.o"
+  "CMakeFiles/gmxfort.dir/utils.f90.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/gmxfort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ xdr_xtc_lib/libgmxfort/build/src/CMakeFiles/gmxfort.dir/cmake_clean_Fortran.cmake
@@ -0,0 +1,18 @@
+# Remove fortran modules provided by this target.
+FILE(REMOVE
+  "../include/gmxfort_common.mod"
+  "../include/GMXFORT_COMMON.mod"
+  "CMakeFiles/gmxfort.dir/gmxfort_common.mod.stamp"
+
+  "../include/gmxfort_index.mod"
+  "../include/GMXFORT_INDEX.mod"
+  "CMakeFiles/gmxfort.dir/gmxfort_index.mod.stamp"
+
+  "../include/gmxfort_trajectory.mod"
+  "../include/GMXFORT_TRAJECTORY.mod"
+  "CMakeFiles/gmxfort.dir/gmxfort_trajectory.mod.stamp"
+
+  "../include/gmxfort_utils.mod"
+  "../include/GMXFORT_UTILS.mod"
+  "CMakeFiles/gmxfort.dir/gmxfort_utils.mod.stamp"
+  )
